7d0f3b5a5ef093ecf45cb02db7fc8a8fb7044fb7
Honey <7937191+honeyLH@user.noreply.gitee.com>
2020-09-06 19:57:33 +0800

    modify the logic of newProduct

--- albedo-ui/src/store/modules/dub.js
+++ albedo-ui/src/store/modules/dub.js
@@ -6,6 +6,7 @@
         idGlobal : 0,
         productid : 0,
         scriptid:0,
+        productData:[],
         // 保存商品和串词列表
         configData:[],
     }
@@ -26,59 +27,92 @@
             if(state.dubActive-- < 0)
                 state.dubActive = 0
         },
-        // 增加商品和保存修改
         ADD_PRODUCT(state,item){
             let ifAdd = 1;
-            state.configData.filter(i=>{
+            state.productData.filter(i=>{
                 if(item.name===i.name&&item.id===i.id){
                     ifAdd = 0;
                 }
             })
             if(ifAdd===1){
                 item.profileList=[],
-                state.configData.push(item)
+                state.productData.push(item)
             }
             ifAdd=1
-            // 保存修改
-            state.configData.filter((i,id)=>{
+            state.productData.filter((i,id)=>{
                 if(item.globalid-1===id && item.ifModify){
                     item.ifModify = 0;
-                    state.configData[id]=item
+                    state.productData[id]=item
                 }
             })
+            localStorage.setItem('productData',JSON.stringify(state.productData))
+        },
+        DELETE_PRODUCT(state,data){
+            state.productData.filter((item,index)=>{
+                console.log("DELETE_PRODUCT!")
+                if(item.id===data.item_id){
+                    state.productData.splice(index,1)
+                    
+                }
+            })
+            // if(state.productid>0){
+            //     --state.productid
+            // }
+            // state.productData.filter((item,id)=>{
+            //     item.id=id+1
+            //     item.name=`${item.id}号商品`
+            // })
+            localStorage.setItem('productData',JSON.stringify(state.productData))
+            this.commit('DELETE_DATA',data)     
+
+        },
+        // 修改中间配置
+        ADD_CONFIG_DATA(state,list){
+            state.configData = list
             localStorage.setItem('configData',JSON.stringify(state.configData))
         },
         ADD_SCRIPT(state,list){
             state.configData = list
             localStorage.setItem('configData',JSON.stringify(state.configData))           
         },
+        // 删除中间配置
         DELETE_DATA(state,data){
+            let deleteNum=0
             state.configData.filter((item,index)=>{
                 if(item.name===data.name && item.id===data.item_id){
-                    state.configData.splice(index,1)
-                    if(data.name==="商品"){
-                        if(state.productid>0){
-                            --state.productid
-                        }
-                        
-                        state.configData.forEach(item=>{
-                            if(item.name==="商品"&&item.id>data.item_id)
-                            {
-                                item.id--
-                            }
-                        })
-                    }
-                    else{
-                        --state.scriptid
-                        state.configData.forEach(item=>{
-                            if(item.name!=="商品"&&item.id>data.item_id)
-                            {
-                                item.id--
-                            }
-                        })
-                    }
+                    console.log("count:",item.id,"item:",item)
+                    deleteNum++
                 }
-              })
+             })
+             console.log("deleteNum:",deleteNum)
+             for(let i = 0;i < deleteNum;i++)
+             {
+                state.configData.filter((item,index)=>{
+                    if(item.name===data.name && item.id===data.item_id){
+                        console.log("DEL:",data.item_id)
+                        state.configData.splice(index,1)
+                        if(data.name==="商品" && index==0){
+                            state.configData.forEach(item=>{
+                                // if(item.name==="商品"&&item.id>data.item_id)
+                                // {
+                                //     item.id--
+                                // }
+                            })
+                        }
+                        else{
+                            --state.scriptid
+                            // state.configData.forEach(item=>{
+                            //     if(item.name!=="商品"&&item.id>data.item_id)
+                            //     {
+                            //         item.id--
+                            //     }
+                            // })
+                        }
+                    }
+                  })
+             }
+            
+
             localStorage.setItem('configData',JSON.stringify(state.configData))
         },
         SUBMIT_DATA(state){
