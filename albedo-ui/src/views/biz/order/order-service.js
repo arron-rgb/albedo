2943c7f41e1ee0d9752dce45cf6e010ec4473120
arronshentu <shentu.k@northeastern.edu>
2020-09-08 11:17:36 +0800

    feat: 后台订单页面

---
+++ albedo-ui/src/views/biz/order/order-service.js
@@ -0,0 +1,36 @@
+import request from '@/utils/request'
+import qs from 'qs'
+
+
+export function page(params) {
+  return request({
+    url: '/biz/order/?' + qs.stringify(params, { indices: false }),
+    method: 'get'
+  })
+}
+
+export function save(data) {
+  return request({
+    url: '/biz/order',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: '/biz/order',
+    method: 'delete',
+    data: ids
+  })
+}
+
+
+export function get(id) {
+  return request({
+    url: '/biz/order/' + id,
+    method: 'get'
+  })
+}
+
+export default { page, del, save, get }
