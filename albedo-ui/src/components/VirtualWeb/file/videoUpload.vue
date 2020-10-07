aafb8e30b9484f7bdfd8c75b54b32950df5f3db2
WillemGavin <1291122461@qq.com>
2020-10-07 22:55:49 +0800

    更新员工接单模块

--- albedo-ui/src/components/VirtualWeb/file/videoUpload.vue
+++ albedo-ui/src/components/VirtualWeb/file/videoUpload.vue
@@ -1,44 +1,20 @@
 <template>
   <div class="uploadContainer">
-    <div class="uploadCard">
-      <!-- <div class="uploadTitle">
-          上传配音
-      </div> -->
-      <el-upload
-        class="upload-demo"
-        ref="upload"
-        action="#"
-        :show-file-list="false"
-        :before-upload="beforeUpload"
-        :auto-upload="false"
-        :multiple="false"
-        :on-change="onUploadChange"
-        drag>
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">格式支持：mp4</div>
-      </el-upload>
-    </div>
-    <el-button type="primary" style="margin-top: 20px" :loading="loading"  @click="next">提交视频</el-button>
-
-    <!--遮罩层-->
-    <div class="loading" v-if="loading" >
-      <h4 class="tips">{{tips}}</h4>
-      <!--进度条-->
-      <el-progress type="line" :percentage="percentage" color="#ff5000" class="progress" :show-text="true"></el-progress>
-    </div>
-    <!--上传完成提示对话框-->
-<!--    <el-dialog-->
-<!--      title="提示"-->
-<!--      :visible="dialogVisible"-->
-<!--      width="30%"-->
-<!--      :modal-append-to-body='false'-->
-<!--    >-->
-<!--      <span>{{tips}}</span>-->
-<!--      <span slot="footer" class="dialog-footer">-->
-<!--        <el-button type="primary" @click="ensure">确 定</el-button>-->
-<!--      </span>-->
-<!--    </el-dialog>-->
+    <el-upload
+      :auto-upload="false"
+      :data="{ orderId : this.videoId}"
+      :file-list="fileList"
+      :on-change="onUploadChange"
+      action="/a/biz/order/upload"
+      class="upload-demo"
+      drag
+      multiple
+      ref="upload">
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      <div class="el-upload__tip" slot="tip">只能上传mp4文件，且不超过4GB</div>
+    </el-upload>
+    <el-button @click="submitUpload" size="small" style="margin-left: 10px;" type="primary">上传到服务器</el-button>
   </div>
 </template>
 
@@ -51,21 +27,13 @@ import aplayer from 'vue-aplayer'
 import storeApi from "@/utils/store";
 
 export default {
-  name: "audioUpdate",
+  name: "videoUpload",
   data(){
     return {
-      url : '',
-      name : '',
-      loading : false,
       videoId : '',
-      percentage:0,
-      tips:'',
-      // dialogVisible:false,
-      state : 0,
+      fileList: [],
+
     }
-  },
-  components :{
-    aplayer
   },
   created() {
     var orderData = storeApi.get({
@@ -79,140 +47,28 @@ export default {
         }
       });
     }else{
-      this.videoId = orderData.videoId;
+      this.videoId = orderData.id;
     }
   },
   methods :{
-    next() {//上传音频
-      this.loading = true;
-      // 检查是否上传了音频
-      // console.log(this.url);
-      if(this.url === null || this.url === undefined || this.url === ''){
-        this.$message.error('请先上传音频！');
-        this.loading = false;
-        return ;
-      }
-      // 调用上传方法
+    submitUpload() {
       this.$refs.upload.submit();
-      this.tips = '正在上传中……';
-    },
-    beforeUpload(file){
-      let fd = new FormData();
-      fd.append('file', file);
-      let config = {
-        onUploadProgress: progressEvent => {
-          //progressEvent.loaded:已上传文件大小
-          //progressEvent.total:被上传文件的总大小
-          let complete = (progressEvent.loaded / progressEvent.total ).toFixed(2) * 100 ;
-          this.percentage = complete;
-        },
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      };
-
-      axios.post('/a/file/upload',fd,config)
-        .then((res)=>{
-          // console.log(res);
-          if(res.data.code === MSG_TYPE_SUCCESS) {//文件上传成功
-            this.saveAudio(res.data.data.url);
-            this.$notify({
-              message: '文件上传成功！',
-            });
-          }
-        })
-        .catch((err)=>{
-          this.$notify({
-            message: '文件上传失败，请稍后重试！',
-          });
-        })
-    },
-    ensure(){
-      this.dialogVisible = false;
-      this.loading = false;
     },
     onUploadChange(file) {//检查文件是否合法
 
       this.name = file.name;
-      const isIMAGE = (file.raw.type === 'audio/mpeg');
-      const isLt1M = file.size / 1024 / 1024 < 200;
+      const isIMAGE = (file.raw.type === 'video/mp4');
+      const isLt1M = file.size / 1024 / 1024 / 1024 < 4;
 
       if (!isIMAGE) {
-        this.$message.error('只能上传mp3格式的音频！');
+        this.$message.error('只能上传mp4格式的视频！');
         return;
       }
       if (!isLt1M) {
-        this.$message.error('上传文件大小不能超过200MB！');
+        this.$message.error('上传文件大小不能超过4GB！');
         return;
       }
-
-      var _this = this;
-
-      var reader = new FileReader();
-      reader.readAsDataURL(file.raw)
-      reader.onload = function (e) {
-        //this.result为图片的base64
-        // console.log(e.target.result)
-        //将图片路径赋值给url
-
-        _this.url = e.target.result;
-      }
-
-    },
-    // uploadAud(file) {//上传文件
-    //   var _this = this
-    //   let config = {
-    //     onUploadProgress: progressEvent => {
-    //       //progressEvent.loaded:已上传文件大小
-    //       //progressEvent.total:被上传文件的总大小
-    //       let complete = (progressEvent.loaded / progressEvent.total ).toFixed(2) * 100 ;
-    //       this.percentage = complete;
-    //       if (this.percentage >= 100){
-    //         this.dialogVisible = true
-    //       }
-    //     },
-    //     headers: {
-    //       'Content-Type': 'multipart/form-data'
-    //     }
-    //   };
-    //   console.log('fdasdfa');
-    //   return new Promise((resolve, reject) => {
-    //     dubOperate.uploadFile(file, config).then(res => {
-    //       if (res.code === MSG_TYPE_SUCCESS) {
-    //         //音频上传成功，二次保存订单
-    //         console.log(res.data.url);
-    //         this.saveAudio(res.data.url);
-    //       }
-    //     }).catch(error => {
-    //       this.loading = false;
-    //       reject(error)
-    //     })
-    //   });
-    // },
-    saveAudio(url){
-      var data = {
-        orderId : this.videoId,//视频订单id
-        audioUrl: url //音频url
-      }
-      return new Promise((resolve, reject) => {
-        crudOrder.uploadAudio(data).then( res => {
-          resolve();
-          this.$alert('音频保存成功，辛苦了！', '提示',{
-            confirmButtonText: '确定',
-            callback: action => {
-              this.goTo('/order/order')
-            }
-          });
-        }).catch(error => {
-          reject(error);
-        })
-      })
-    },
-    goTo(url, data){
-      //带参数跳转
-      // console.log(data)
-      this.$router.push({path:url, query : {data: data}});
-    },
+    }
   }
 }
 </script>
@@ -220,33 +76,7 @@ export default {
 <style scoped>
 .uploadContainer{
   width: 80%;
-  margin: auto;
-}
-
-.loading{
-  position: absolute;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  background: black;
-  opacity: 0.8;
-}
-.progress{
-  width: 200px;
-  height: 200px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin-left: -100px;
-  margin-top: -100px;
-}
-.tips{
-  color: white;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin-left: -100px;
-  margin-top: -150px;
+  margin: 20px auto;
+  text-align: center;
 }
 </style>
--- albedo-ui/src/views/biz/order/orderDetail.vue
+++ albedo-ui/src/views/biz/order/orderDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    {{this.orderData}}
+<!--    {{this.orderData}}-->
 <!--    视频订单详情查看-->
     <el-card class="box-card" body-style="padding : 0px" v-if="orderData.type === '2'">
       <div slot="header" class="clearfix">
@@ -41,7 +41,7 @@
       </el-row>
 
 <!--      <el-row class="box" style="text-align: center" v-if="orderData.state === 2">-->
-      <myaudio v-if="orderData.state === 2"></myaudio>
+      <audio-upload v-if="orderData.state === 2"></audio-upload>
 <!--      </el-row>-->
     </el-card>
 
@@ -123,19 +123,16 @@
           产品视频
         </el-col>
         <el-col span="20">
-          <div class='videoPlayer'>
-            <video-player  class="video-player vjs-custom-skin"
-                           ref="videoPlayer"
-                           :playsinline="true"
-                           :options="playerOptions">
-            </video-player>
-          </div>
-
+          <my-video></my-video>
         </el-col>
       </el-row>
 
       <el-row class="box" style="text-align: center" v-if="orderData.state === 1">
           <el-button @click="takeOrder(orderData.id)" :loading="loading" type="primary" style="width: 150px">接单</el-button>
+      </el-row>
+
+      <el-row class="box" v-if="orderData.state === 2 || orderData.state === 4">
+        <video-upload></video-upload>
       </el-row>
     </el-card>
   </div>
@@ -145,7 +142,9 @@
 import storeApi from "@/utils/store";
 import crudOrder from '@/views/biz/order/order-service'
 import {MSG_TYPE_SUCCESS} from "@/const/common";
-import myaudio from '@/components/VirtualWeb/file/audioUpload'
+import audioUpload from '@/components/VirtualWeb/file/audioUpload'
+import videoUpload from '@/components/VirtualWeb/file/videoUpload'
+import myVideo from '@/components/VirtualWeb/file/video'
 import MyOrder from "@/views/VirtualWeb/personal/myOrder";
 
 
@@ -153,7 +152,9 @@ export default {
   name: "orderDetail",
   components: {
     MyOrder,
-    myaudio
+    audioUpload,
+    videoUpload,
+    myVideo
   },
   data(){
     return {
@@ -161,31 +162,6 @@ export default {
       loading : false,
       tagList : ["", "success", "info", "warning", "danger"],
       voiceData : [],
-      playerOptions : {
-        playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
-        autoplay: false, //如果true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
-        loop: false, // 导致视频一结束就重新开始。
-        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        // language: 'zh-CN',
-        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [{
-          type: "video/mp4",
-          src: 'http://' + this.orderData.videoId//url地址
-
-        }],
-        // poster: require("@/assets/VirtualWeb/NormalUser/img/videoCover.jpg"), //你的封面地址
-        // width: document.documentElement.clientWidth,
-        notSupportedMessage: '此视频暂无法播放，请稍后再试',
-        //允许覆盖Video.js无法播放媒体源时显示的默认信息。
-        controlBar: {
-          timeDivider: true,
-          durationDisplay: true,
-          remainingTimeDisplay: false,
-          fullscreenToggle: false  //全屏按钮
-        }
-      }
     }
   },
   created() {
