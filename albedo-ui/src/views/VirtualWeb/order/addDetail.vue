a64cdc3a473839e5343e5935fafa574342521a31
WillemGavin <1291122461@qq.com>
2020-10-18 21:57:12 +0800

    修复addDetail的bug

--- albedo-ui/src/views/VirtualWeb/order/addDetail.vue
+++ albedo-ui/src/views/VirtualWeb/order/addDetail.vue
@@ -13,7 +13,7 @@
   </div>
 
   <!-- 反馈信息 -->
-  <div class="descriBlock" v-if="this.balance.editTimes > 0">
+  <div class="descriBlock" v-if="this.balance.editTimes !== null && this.balance.editTimes > 0">
     <div class="startBar">
       <el-row  class="box">
         <el-col span="4">
